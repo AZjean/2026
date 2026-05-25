39ccdf199d2e91b60b6b83ffdae5ea22c969eec0
AZjean <zhuyan00329@gmail.com>
2026-05-25 17:00:45 +0800

    视频优化

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const nextConfig = {
   },
   images: {
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
